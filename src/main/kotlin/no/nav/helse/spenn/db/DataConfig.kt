7e92774d71ac6ac2f321293d497a1eff088a1fb4
Håkon Arneng Holmstedt <Hakon.Arneng.Holmstedt@nav.no>
2019-04-02 12:02:38 +0200

    Remove redundant @EnableJdbcRepo, and renamed bean _from_ flyway since this is already a spring bean.

--- src/main/kotlin/no/nav/helse/spenn/db/DataConfig.kt
+++ src/main/kotlin/no/nav/helse/spenn/db/DataConfig.kt
@@ -1,17 +1,17 @@
 package no.nav.helse.spenn.db
 
 import no.nav.helse.Environment
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories
+import javax.sql.DataSource
 
 @Configuration
-@EnableJdbcRepositories
-class DataConfig(val env: Environment) {
+class DataConfig(val env: Environment, @Autowired val dataSource: DataSource) {
 
     @Bean
-    fun flyway(): FlywayConfigurationCustomizer {
+    fun flywayConfig(): FlywayConfigurationCustomizer {
         return FlywayConfigurationCustomizer { configuration ->
             configuration.initSql(String.format("SET ROLE \"%s-admin\"", env.oppdragDBNavn))
         }
