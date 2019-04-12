06da2c86430bf163a1db4badc8856af053c71a75
Håkon Arneng Holmstedt <Hakon.Arneng.Holmstedt@nav.no>
2019-04-12 11:34:25 +0200

    flyttet all vedtak-mapping til samme fil; alt er extension-functions

---
+++ src/main/kotlin/no/nav/helse/spenn/vedtak/Mapping.kt
@@ -0,0 +1,78 @@
+package no.nav.helse.spenn.vedtak
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.ArrayNode
+import no.nav.helse.integrasjon.okonomi.oppdrag.AksjonsKode
+import no.nav.helse.integrasjon.okonomi.oppdrag.SatsTypeKode
+import no.nav.helse.spenn.oppdrag.UtbetalingsLinje
+import no.nav.helse.spenn.oppdrag.UtbetalingsOppdrag
+import java.math.BigDecimal
+import java.math.BigInteger
+import java.time.LocalDate
+
+typealias Fodselsnummer = String
+
+interface AktørTilFnrMapper {
+    fun tilFnr(aktørId: String): Fodselsnummer
+}
+
+private class DummyAktørMapper() : AktørTilFnrMapper {
+    override fun tilFnr(aktørId: String): Fodselsnummer = aktørId
+}
+
+fun Vedtak.tilOppdrag(mapper: AktørTilFnrMapper = DummyAktørMapper()): UtbetalingsOppdrag = UtbetalingsOppdrag(
+        id = søknadId,
+        operasjon = AksjonsKode.OPPDATER,
+        oppdragGjelder = mapper.tilFnr(aktørId),
+        utbetalingsLinje = lagLinjer()
+)
+
+private fun Vedtak.lagLinjer(): List<UtbetalingsLinje> =
+        vedtaksperioder.mapIndexed { index, periode ->
+            periode.fordeling.mapIndexed { fordelingsIndex, fordeling ->
+                UtbetalingsLinje(
+                        id = "$index/$fordelingsIndex",
+                        datoFom = periode.fom,
+                        datoTom = periode.tom,
+                        sats = BigDecimal(periode.dagsats),
+                        satsTypeKode = SatsTypeKode.DAGLIG,
+                        utbetalesTil = fordeling.mottager,
+                        grad = BigInteger.valueOf(100)
+                )
+            }
+        }.flatten()
+
+fun JsonNode.tilVedtak(key: String): Vedtak =
+        Vedtak(søknadId = key,
+                aktørId = this.get("originalSøknad").get("aktorId").textValue(),
+                vedtaksperioder = lagVedtaksperioder(this.get("vedtak").get("perioder")))
+
+private fun lagVedtaksperioder(perioderNode: JsonNode): List<Vedtaksperiode> =
+        when (perioderNode) {
+            is ArrayNode -> {
+                perioderNode.map { pNode ->
+                    Vedtaksperiode(
+                            fom = pNode.get("fom").asLocalDate(),
+                            tom = pNode.get("tom").asLocalDate(),
+                            dagsats = pNode.get("dagsats").intValue(),
+                            fordeling = lagFordelinger(pNode.get("fordeling"))
+                    )
+                }
+            }
+            else -> emptyList()
+        }
+
+private fun lagFordelinger(fordelingsNode: JsonNode): List<Fordeling> =
+        when (fordelingsNode) {
+            is ArrayNode -> {
+                fordelingsNode.map { fNode ->
+                    Fordeling(
+                            mottager = fNode.get("mottager").textValue(),
+                            andel = fNode.get("andel").intValue()
+                    )
+                }
+            }
+            else -> emptyList()
+        }
+
+private fun JsonNode.asLocalDate(): LocalDate = LocalDate.parse(textValue())
