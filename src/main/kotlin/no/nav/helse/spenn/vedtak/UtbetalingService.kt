51c3b3275227a47c8f544f118320bb989486bcdc
Håkon Arneng Holmstedt <Hakon.Arneng.Holmstedt@nav.no>
2019-04-12 12:06:05 +0200

    autowire avhengighetene til utbetalingService

--- src/main/kotlin/no/nav/helse/spenn/vedtak/UtbetalingService.kt
+++ src/main/kotlin/no/nav/helse/spenn/vedtak/UtbetalingService.kt
@@ -6,12 +6,14 @@ import no.nav.helse.spenn.oppdrag.OppdragMapper
 import no.nav.helse.spenn.oppdrag.UtbetalingsOppdrag
 import no.nav.helse.spenn.simulering.OppdragMapperForSimulering
 import no.nav.helse.spenn.simulering.SimuleringService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 @Component
-class UtbetalingService(val simuleringService: SimuleringService,
-                        val oppdragMapperForSimulering: OppdragMapperForSimulering,
-                        val oppdragSender: OppdragMQSender, val oppdragMapper: OppdragMapper) {
+class UtbetalingService(@Autowired val simuleringService: SimuleringService,
+                        @Autowired val oppdragMapperForSimulering: OppdragMapperForSimulering,
+                        @Autowired val oppdragSender: OppdragMQSender,
+                        @Autowired val oppdragMapper: OppdragMapper) {
 
     fun sendUtbetalingToOS(utbetaling: UtbetalingsOppdrag) {
         if (utbetaling.operasjon == AksjonsKode.SIMULERING) {
