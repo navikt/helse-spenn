818f9e1c6d938a41b26f7b6d5054e17c2a3abb25
tuantrannav <dinh.anh.tuan.tran@nav.no>
2019-04-25 12:06:01 +0200

    remove more travis stuff

--- triggerbuild.sh
+++
@@ -1,5 +0,0 @@
-q#!/bin/sh
-PROJECT=$@
-echo "Trigger build $PROJECT"
-output=$(curl -X POST -H "Content-Type: application/json" -H "Accept: application/json" -H "Travis-API-Version: 3" -H "Authorization: token $TRAVIS_API_TOKEN" -d "{\"request\": {\"branch\":\"master\"}}" "https://api.travis-ci.com/repo/navikt%2F$PROJECT/requests")
-echo $output
