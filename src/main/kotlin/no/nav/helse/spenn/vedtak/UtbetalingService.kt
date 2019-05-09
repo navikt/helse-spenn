5a945ad6d197d48996f17e48741f6247d8f35a6b
Tuan Tran <Dinh.Anh.Tuan.Tran@nav.no>
2019-05-09 11:54:57 +0200

    metrics for utbetalt belop

--- src/main/kotlin/no/nav/helse/spenn/vedtak/UtbetalingService.kt
+++ src/main/kotlin/no/nav/helse/spenn/vedtak/UtbetalingService.kt
@@ -4,9 +4,12 @@ import io.micrometer.core.instrument.MeterRegistry
 import no.nav.helse.spenn.dao.OppdragStateService
 import no.nav.helse.spenn.dao.OppdragStateStatus
 import no.nav.helse.spenn.metrics.SIMULERING
+import no.nav.helse.spenn.metrics.SIMULERING_UTBETALT_BELOP
+import no.nav.helse.spenn.metrics.SIMULERING_UTBETALT_TOTAL_BELOP
 import no.nav.helse.spenn.oppdrag.OppdragMQSender
 import no.nav.helse.spenn.oppdrag.toOppdrag
 import no.nav.helse.spenn.oppdrag.toSimuleringRequest
+import no.nav.helse.spenn.simulering.SimuleringResult
 import no.nav.helse.spenn.simulering.SimuleringService
 import no.nav.helse.spenn.simulering.Status
 import org.slf4j.LoggerFactory
@@ -17,7 +20,6 @@ import org.springframework.stereotype.Service
 class UtbetalingService(val simuleringService: SimuleringService,
                         val oppdragSender: OppdragMQSender,
                         val oppdragStateService: OppdragStateService,
-                        val aktørTilFnrMapper: AktørTilFnrMapper,
                         val meterRegistry: MeterRegistry) {
 
     private val log = LoggerFactory.getLogger(UtbetalingService::class.java)
@@ -31,7 +33,7 @@ class UtbetalingService(val simuleringService: SimuleringService,
             else -> OppdragStateStatus.FEIL
         }
         oppdrag.simuleringResult = result
-        meterRegistry.counter(SIMULERING, "status", oppdrag.status.name).increment()
+        simuleringMetrics(result)
         return oppdragStateService.saveOppdragState(oppdrag)
     }
 
@@ -42,5 +44,12 @@ class UtbetalingService(val simuleringService: SimuleringService,
 
     }
 
+    private fun simuleringMetrics(result: SimuleringResult) {
+        if (result.status == Status.OK) {
+            meterRegistry.counter(SIMULERING_UTBETALT_TOTAL_BELOP).increment(result.mottaker!!.totalBelop.toDouble())
+            meterRegistry.gauge(SIMULERING_UTBETALT_BELOP, result.mottaker!!.totalBelop)
+        }
+        meterRegistry.counter(SIMULERING, "status", result.status.name).increment()
+    }
 
 }
