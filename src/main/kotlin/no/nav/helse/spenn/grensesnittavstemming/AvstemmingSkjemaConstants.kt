f8aa3f950162fa177cee412a4428f74d07d49aa1
royag-nav <roy.andre.gundersen@nav.no>
2019-06-07 15:02:46 +0200

    Legg inn XSD for avstemming og test grunnleggende de+serialisering. xsd er kopiert fra VL-FP, den hadde en (modifisert?) xsd + en katalog med "xsd-original". Legger inn på samme måte her inntil videre i allefall, sånn at en husker at det kan være noe "greier" der.

---
+++ src/main/kotlin/no/nav/helse/spenn/grensesnittavstemming/AvstemmingSkjemaConstants.kt
@@ -0,0 +1,12 @@
+package no.nav.helse.spenn.grensesnittavstemming
+
+import no.nav.virksomhet.tjenester.avstemming.meldinger.v1.Avstemmingsdata
+
+class AvstemmingSkjemaConstants {
+
+    companion object {
+        @JvmField
+        val JAXB_CLASS = Avstemmingsdata::class.java
+    }
+}
+
