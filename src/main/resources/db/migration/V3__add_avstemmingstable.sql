06f995b149ebce2031d590a6cabdfe85278f7687
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2019-06-26 12:25:56 +0200

    added migration script for avstemmingtable

--- src/main/resources/db/migration/V3__add_avstemmingstable.sql
+++ src/main/resources/db/migration/V3__add_avstemmingstable.sql
@@ -1,6 +1,13 @@
+alter table oppdragstate drop column avstemmingsnokkel;
+
+create sequence avstemming_id_seq;
+
 create table avstemming(
-    oppdragstate_id bigint not null foreign key (oppdragstate_id) references oppdragstate(id) unique,
+    id bigint not null default nextval('avstemming_id_seq'),
+    oppdragstate_id bigint not null references oppdragstate(id) ON DELETE CASCADE ON UPDATE CASCADE,
     nokkel timestamp not null,
     avstemt boolean not null,
-    constraint uq_avstemming_nokkel unique (nokkel)
-)
+    primary key (id),
+    constraint uq_avstemming_nokkel unique (nokkel),
+    constraint uq_avstemming_oppdragstate unique (oppdragstate_id)
+);
