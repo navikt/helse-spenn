ebd68f863b2bf52b2a25ba65f08b33c2780b168a
royag-nav <roy.andre.gundersen@nav.no>
2019-08-06 14:38:06 +0200

    publiser hendelser for ferdige(/aktiverte) oppdrag

---
+++ src/main/kotlin/no/nav/helse/spenn/oppdrag/OppdragStateKafkaProducer.kt
@@ -0,0 +1,22 @@
+package no.nav.helse.spenn.oppdrag
+
+import no.nav.helse.spenn.vedtak.defaultObjectMapper
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.stereotype.Component
+
+data class OppdragFerdigInfo(val soknadId:String)
+
+@Component
+class OppdragStateKafkaProducer(val kafkaTemplate : KafkaTemplate<String, String>,
+                                @Value("\${oppdragstate.topic.send}") val topic:String) {
+
+    private val log = LoggerFactory.getLogger(OppdragStateKafkaProducer::class.java)
+
+    fun send(info : OppdragFerdigInfo) {
+        val message = defaultObjectMapper.writeValueAsString(info)
+        log.debug("sending message='{}' to topic='{}'", message, topic)
+        kafkaTemplate.send(topic, info.soknadId, message)
+    }
+}
