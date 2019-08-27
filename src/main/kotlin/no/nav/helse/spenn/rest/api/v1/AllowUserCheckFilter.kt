eba6ba88a2dd37c0e9bda08e63f165cd009499c5
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2019-08-27 16:26:00 +0200

    filter only /api

--- src/main/kotlin/no/nav/helse/spenn/rest/api/v1/AllowUserCheckFilter.kt
+++ src/main/kotlin/no/nav/helse/spenn/rest/api/v1/AllowUserCheckFilter.kt
@@ -3,28 +3,28 @@ package no.nav.helse.spenn.rest.api.v1
 import no.nav.security.oidc.context.OIDCRequestContextHolder
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import javax.servlet.Filter
+import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.FilterChain
-import javax.servlet.ServletRequest
-import javax.servlet.ServletResponse
+import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
-
-
-
 
 
 @Component
 class AllowUserCheckFilter(val oidcRequestContextHolder: OIDCRequestContextHolder,
-                           @Value("\${api.access.requiredgroup:group1}") val requiredGroupMembership: String): Filter {
+                           @Value("\${api.access.requiredgroup:group1}") val requiredGroupMembership: String): OncePerRequestFilter() {
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
 
-    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
         if (ourIssuer() == null || currentUserGroups() == null || (!currentUserGroups().contains(requiredGroupMembership))) {
-            (response as HttpServletResponse).sendError(HttpServletResponse.SC_UNAUTHORIZED, "Missing group $requiredGroupMembership in JWT")
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Missing group $requiredGroupMembership in JWT")
         }
         else {
-            chain.doFilter(request,response)
+            filterChain.doFilter(request,response)
         }
+    }
 
+    override fun shouldNotFilter(request: HttpServletRequest): Boolean {
+        val path = request.requestURI.substring(request.contextPath.length)
+        return !path.startsWith("/api/")
     }
 
     private fun ourIssuer () = oidcRequestContextHolder.oidcValidationContext.getClaims("ourissuer")
