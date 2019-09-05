ac547ed0644da6580b0598af87e0a94f1cc3d58c
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2019-09-05 11:06:04 +0200

    temp disable a test

--- src/test/kotlin/no/nav/helse/spenn/rest/SimuleringControllerTest.kt
+++ src/test/kotlin/no/nav/helse/spenn/rest/SimuleringControllerTest.kt
@@ -12,29 +12,29 @@ import org.springframework.test.web.servlet.MockMvc
 
 class SimuleringControllerTest {
 
-    @Autowired
-    lateinit var mockMvc: MockMvc
-
-    companion object {
-        val server: WireMockServer = WireMockServer(WireMockConfiguration.options().port(33333))
-        @BeforeAll
-        @JvmStatic
-        fun before() {
-            server.start()
-            WireMock.configureFor(server.port())
-            stubOIDCProvider()
-        }
-        @AfterAll
-        @JvmStatic
-        fun after() {
-            server.stop()
-        }
-    }
-
-    @Test
-    fun runSimulering() {
-
-    }
+//    @Autowired
+//    lateinit var mockMvc: MockMvc
+//
+//    companion object {
+//        val server: WireMockServer = WireMockServer(WireMockConfiguration.options().port(33333))
+//        @BeforeAll
+//        @JvmStatic
+//        fun before() {
+//            server.start()
+//            WireMock.configureFor(server.port())
+//            stubOIDCProvider()
+//        }
+//        @AfterAll
+//        @JvmStatic
+//        fun after() {
+//            server.stop()
+//        }
+//    }
+//
+//    @Test
+//    fun runSimulering() {
+//
+//    }
 }
 
 val etVedtakJson = """{"soknadId":"9ada4305-1e45-4d48-ba48-a504bc96040d","aktorId":"en random aktørid","vedtaksperioder":[{"fom":"2020-01-15","tom":"2020-01-30","grad":100,"dagsats":1234,"fordeling":[{"mottager":"897654321","andel":100}]}],"maksDato":"2020-09-03"}"""
