433c46acb0839959743ad876a7aa540dca43a5a4
Tuan Tran <Dinh.Anh.Tuan.Tran@nav.no>
2019-09-09 10:52:36 +0200

    ignoreunknown

--- src/main/kotlin/no/nav/helse/spenn/oppdrag/UtbetalingsOppdrag.kt
+++ src/main/kotlin/no/nav/helse/spenn/oppdrag/UtbetalingsOppdrag.kt
@@ -1,20 +1,22 @@
 package no.nav.helse.spenn.oppdrag
 
-import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import no.nav.helse.spenn.vedtak.Vedtak
+
 
 import java.math.BigDecimal
 import java.math.BigInteger
 import java.time.LocalDate
 
 
-
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class UtbetalingsOppdrag(
         val vedtak: Vedtak,
         val operasjon : AksjonsKode,
         val oppdragGjelder: String, // "angir hvem som saken/vedtaket er registrert på i fagrutinen"
         val utbetalingsLinje : List<UtbetalingsLinje>)
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class UtbetalingsLinje(val id: String, // delytelseId - "fagsystemets entydige identifikasjon av oppdragslinjen"
                             val sats: BigDecimal,
                             val satsTypeKode: SatsTypeKode,
