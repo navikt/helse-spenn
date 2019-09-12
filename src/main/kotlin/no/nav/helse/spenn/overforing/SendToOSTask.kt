6bf227b9602efa8f700adb25b157c9039086b776
royag-nav <roy.andre.gundersen@nav.no>
2019-09-12 11:31:41 +0200

    sanityCheck på dagsats

--- src/main/kotlin/no/nav/helse/spenn/overforing/SendToOSTask.kt
+++ src/main/kotlin/no/nav/helse/spenn/overforing/SendToOSTask.kt
@@ -3,15 +3,17 @@ package no.nav.helse.spenn.overforing
 import io.micrometer.core.instrument.MeterRegistry
 import net.javacrumbs.shedlock.core.SchedulerLock
 import no.nav.helse.spenn.appsupport.OPPDRAG
+import no.nav.helse.spenn.oppdrag.AvstemmingDTO
+import no.nav.helse.spenn.oppdrag.OppdragStateDTO
 import no.nav.helse.spenn.oppdrag.dao.OppdragStateService
 import no.nav.helse.spenn.oppdrag.dao.OppdragStateStatus
-import no.nav.helse.spenn.oppdrag.AvstemmingDTO
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Profile
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.math.BigDecimal
 
 
 @Component
@@ -28,16 +30,33 @@ class SendToOSTask(val oppdragStateService: OppdragStateService,
     @SchedulerLock(name = "sendToOS")
     fun sendToOS() {
         val oppdragList = oppdragStateService.fetchOppdragStateByStatus(OppdragStateStatus.SIMULERING_OK,limit)
+        if (oppdragList.size >= limit) {
+            log.warn("Det ble hentet ut ${oppdragList.size} oppdrag, som er maksGrensen, kanskje klarer vi ikke å få unna alle?")
+        }
         log.info("We are sending ${oppdragList.size} to OS")
 
         oppdragList.forEach {
-            val updated = it.copy(status = OppdragStateStatus.SENDT_OS, avstemming = AvstemmingDTO())
-            oppdragStateService.saveOppdragState(updated)
-            utbetalingService.sendUtbetalingOppdragMQ(updated)
-            meterRegistry.counter(OPPDRAG,"status", OppdragStateStatus.SENDT_OS.name).increment()
-
+            if (passesSanityCheck(it)) {
+                val updated = it.copy(status = OppdragStateStatus.SENDT_OS, avstemming = AvstemmingDTO())
+                oppdragStateService.saveOppdragState(updated)
+                utbetalingService.sendUtbetalingOppdragMQ(updated)
+                meterRegistry.counter(OPPDRAG, "status", OppdragStateStatus.SENDT_OS.name).increment()
+            } else {
+                log.error("oppdrag med soknadId=${it.soknadId} bestod ikke sanityCheck!")
+            }
         }
 
+    }
+
+    fun passesSanityCheck(oppdragDTO: OppdragStateDTO) : Boolean {
+        val maksDagsats = BigDecimal(99858 * 6.5 / 260) // TODO ?
+        oppdragDTO.utbetalingsOppdrag.utbetalingsLinje.forEach {
+            if (it.sats > maksDagsats) {
+                log.error("sats i oppdrag med soknadId=${oppdragDTO.soknadId} er ${it.sats} som er høyere enn begrensningen på $maksDagsats")
+                return false
+            }
+        }
+        return true
     }
 
 }
