fe23a502c5504f58c5245588cbf9399caaf1a910
royag-nav <roy.andre.gundersen@nav.no>
2019-10-18 17:23:45 +0200

    sts rest client

--- src/main/kotlin/no/nav/helse/spenn/vedtak/fnr/StsRestClient.kt
+++ src/main/kotlin/no/nav/helse/spenn/vedtak/fnr/StsRestClient.kt
@@ -1,45 +1,45 @@
 package no.nav.helse.spenn.vedtak.fnr
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.annotation.JsonProperty
+import org.json.JSONObject
 import java.time.LocalDateTime
-
+import java.util.*
 
 //@Component
-class StsRestClient(/*@Value("\${SECURITY_TOKEN_SERVICE_REST_URL}")*/ val stsRestUrl: String,
+/*class StsRestClient(/*@Value("\${SECURITY_TOKEN_SERVICE_REST_URL}")*/ val stsRestUrl: String,
                     /*@Value("\${STS_REST_USERNAME}")*/ val stsRestUsername: String,
-                    /*@Value("\${STS_REST_PASSWORD}")*/ val stsRestPassword: String) {
+                    /*@Value("\${STS_REST_PASSWORD}")*/ val stsRestPassword: String) {*/
+class StsRestClient(val baseUrl: String, val username: String, val password: String) {
     private var cachedOidcToken: Token? = null
 
     fun token(): String {
-        /*if (Token.shouldRenew(cachedOidcToken))  {
-           val webClient = WebClient.builder().baseUrl(stsRestUrl)
-                   .filter(ExchangeFilterFunctions.basicAuthentication(stsRestUsername, stsRestPassword))
-                   .build()
-            cachedOidcToken = webClient.get().uri("/rest/v1/sts/token?grant_type=client_credentials&scope=openid")
-                    .accept(MediaType.APPLICATION_JSON)
-                    .retrieve()
-                    .bodyToMono(Token::class.java)
-                    .block()
-        }*/
+        if (Token.shouldRenew(cachedOidcToken)) {
+            val encodedAuth = String(Base64.getEncoder().encode("$username:$password".toByteArray()))
+            cachedOidcToken =
+                    khttp.get(
+                            url = "$baseUrl/rest/v1/sts/token?grant_type=client_credentials&scope=openid",
+                            headers = mapOf(
+                                    "Authorization" to "Basic $encodedAuth",
+                                    "Accept" to "application/json"
+                            )).jsonObject.mapToToken()
+        }
         return cachedOidcToken!!.accessToken
     }
 
-    @JsonIgnoreProperties(ignoreUnknown = true)
-    data class Token(@JsonProperty("access_token") val accessToken: String, @JsonProperty("token_type") val type: String, @JsonProperty("expires_in") val expiresIn: Int) {
+    private fun JSONObject.mapToToken(): Token {
+        return Token(getString("access_token"),
+                getString("token_type"),
+                getInt("expires_in"))
+    }
+
+    data class Token(val accessToken: String, val type: String, val expiresIn: Int) {
         // expire 10 seconds before actual expiry. for great margins.
         val expirationTime: LocalDateTime = LocalDateTime.now().plusSeconds(expiresIn - 10L)
 
         companion object {
-            fun shouldRenew(token: Token?): Boolean {
-                if (token == null) {
-                    return true
-                }
+            fun shouldRenew(token: Token?) =
+                    token == null || isExpired(token)
 
-                return isExpired(token)
-            }
-
-            fun isExpired(token: Token): Boolean {
+            private fun isExpired(token: Token): Boolean {
                 return token.expirationTime.isBefore(LocalDateTime.now())
             }
         }
