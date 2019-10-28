8d222c4b28ab54739c88051b65dac3427528969e
royag-nav <roy.andre.gundersen@nav.no>
2019-10-28 11:48:51 +0100

    meir dibøgg

--- src/main/kotlin/no/nav/helse/spenn/vedtak/fnr/StsRestClient.kt
+++ src/main/kotlin/no/nav/helse/spenn/vedtak/fnr/StsRestClient.kt
@@ -1,6 +1,8 @@
 package no.nav.helse.spenn.vedtak.fnr
 
+import org.json.JSONException
 import org.json.JSONObject
+import org.slf4j.LoggerFactory
 import java.time.LocalDateTime
 import java.util.*
 
@@ -11,16 +13,30 @@ import java.util.*
 class StsRestClient(val baseUrl: String, val username: String, val password: String) {
     private var cachedOidcToken: Token? = null
 
+    companion object {
+        private val LOG = LoggerFactory.getLogger(AktorRegisteretClient::class.java)
+    }
+
     fun token(): String {
         if (Token.shouldRenew(cachedOidcToken)) {
             val encodedAuth = String(Base64.getEncoder().encode("$username:$password".toByteArray()))
-            cachedOidcToken =
+            val resp =
                     khttp.get(
                             url = "$baseUrl/rest/v1/sts/token?grant_type=client_credentials&scope=openid",
                             headers = mapOf(
                                     "Authorization" to "Basic $encodedAuth",
                                     "Accept" to "application/json"
-                            )).jsonObject.mapToToken()
+                            ))
+
+            if (resp.statusCode != 200) {
+                LOG.error("Got statusCode ${resp.statusCode}")
+            }
+            try {
+                cachedOidcToken = resp.jsonObject.mapToToken()
+            } catch (e: JSONException) {
+                LOG.error("Bad JSON: ${resp.text}")
+                throw e
+            }
         }
         return cachedOidcToken!!.accessToken
     }
