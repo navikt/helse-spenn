5aba0a0bcaf0e5051b54b5cadf25d1e94b02c02e
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-11-16 13:53:18 +0100

    Blackbox-test som spinner opp spenn med avhengigheter som containers
    Co-authored-by: Stephen Ramthun <stephen.ramthun@nav.no>

---
+++ src/test/resources/blackbox_test-setup-vault.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+set -e
+
+_postgresHost="$1"
+_postgresUsername="$2"
+_postgresPassword="$3"
+
+vault secrets enable -path=postgresql/preprod-fss database
+vault write postgresql/preprod-fss/config/helse-spenn-oppdrag plugin_name=postgresql-database-plugin allowed_roles=helse-spenn-oppdrag-admin,helse-spenn-oppdrag-user connection_url=postgresql://{{username}}:{{password}}@${_postgresHost}?sslmode=disable username=${_postgresUsername} password=${_postgresPassword}
+vault write postgresql/preprod-fss/roles/helse-spenn-oppdrag-admin db_name=helse-spenn-oppdrag creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; GRANT \"helse-spenn-oppdrag-admin\" TO \"{{name}}\";" default_ttl="5m" max_ttl="5m"
+vault write postgresql/preprod-fss/roles/helse-spenn-oppdrag-user db_name=helse-spenn-oppdrag creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; GRANT \"helse-spenn-oppdrag-user\" TO \"{{name}}\";" default_ttl="5m" max_ttl="5m"
+
+echo "Exit OK"
