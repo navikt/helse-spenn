2ae3bc15fc589e3a32e72fc84937deedb87f3a4d
Stephen Ramthun <stephenramthun@gmail.com>
2019-12-05 12:52:28 +0100

    Migrer til nytt schema
    Co-authored-by: Roy Andre Gundersen <roy.andre.gundersen@nav.no>

---
+++ src/main/resources/db/migration/V8__add_oppdrag_and_transaksjon_tables.sql
@@ -0,0 +1,30 @@
+create sequence oppdrag_id_seq;
+create sequence transaksjon_id_seq;
+
+create table oppdrag(
+    id bigint not null default nextval('oppdrag_id_seq'),
+    sakskompleks_id uuid not null,
+    created timestamp not null default now(),
+    modified timestamp not null default now(),
+    utbetalingsreferanse varchar(30) not null unique,
+    constraint pk_oppdrag primary key (id),
+    constraint uq_oppdrag_sakskompleks_id unique (sakskompleks_id)
+);
+
+create table transaksjon(
+    id bigint not null default nextval('transaksjon_id_seq'),
+    oppdrag_id bigint not null references oppdrag(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    nokkel timestamp not null,
+    avstemt boolean not null,
+    status varchar(16) not null,
+    utbetalingsoppdrag text not null,
+    oppdragresponse text,
+    simuleringresult text,
+    primary key (id),
+    constraint uq_transaksjon_nokkel unique (nokkel),
+    constraint uq_transaksjon_oppdrag_id unique (oppdrag_id)
+);
+
+create index oppdrag_modified_idx on oppdrag(modified);
+create index transaksjon_status_idx on transaksjon(status);
+
