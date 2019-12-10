681f0d99fa74d52d305da38b5d34078a4afda341
royag-nav <roy.andre.gundersen@nav.no>
2019-12-10 12:45:56 +0100

    WIP: også spenn-server som egen modul
    Co-Authored-by: Stephen Ramthun <stephen.ramthun@nav.no>

---
+++ spenn-server/src/main/kotlin/no/nav/helse/spenn/simulering/SendToSimuleringTask.kt
@@ -0,0 +1,51 @@
+package no.nav.helse.spenn.simulering
+
+import io.micrometer.core.instrument.MeterRegistry
+import no.nav.helse.spenn.appsupport.SIMULERING
+import no.nav.helse.spenn.appsupport.SIMULERING_UTBETALT_BELOP
+import no.nav.helse.spenn.appsupport.SIMULERING_UTBETALT_MAKS_BELOP
+import no.nav.helse.spenn.oppdrag.dao.OppdragService
+import org.slf4j.LoggerFactory
+import java.util.concurrent.atomic.AtomicLong
+import javax.annotation.PostConstruct
+
+class SendToSimuleringTask(private val simuleringService: SimuleringService,
+                           private val oppdragService: OppdragService,
+                           private val meterRegistry: MeterRegistry,
+                           private val limit: Int = 100) {
+
+    private val maksBelopGauge = AtomicLong(0)
+
+    companion object {
+        private val LOG = LoggerFactory.getLogger(SendToSimuleringTask::class.java)
+    }
+
+    @PostConstruct
+    fun init() {
+        LOG.info("init gauge maksBeløp")
+        meterRegistry.gauge(SIMULERING_UTBETALT_MAKS_BELOP, maksBelopGauge)
+    }
+
+    fun sendSimulering() {
+        LOG.trace("Running SendToSimulering task")
+        val oppdragList = oppdragService.hentNyeBehov(limit)
+        if (oppdragList.isNotEmpty()) {
+            LOG.info("Got ${oppdragList.size} items for simulering")
+        }
+        oppdragList.forEach {transaksjon ->
+            val result = simuleringService.runSimulering(transaksjon)
+
+            transaksjon.oppdaterSimuleringsresultat(result)
+            simuleringMetrics(result)
+        }
+    }
+
+    private fun simuleringMetrics(simuleringResult: SimuleringResult) {
+        val simulering = simuleringResult.simulering
+        if (simuleringResult.status == SimuleringStatus.OK && simulering != null) {
+            meterRegistry.counter(SIMULERING_UTBETALT_BELOP).increment(simulering.totalBelop.toDouble())
+            maksBelopGauge.set(simulering.totalBelop.toLong())
+        }
+        meterRegistry.counter(SIMULERING, "status", simuleringResult.status.name).increment()
+    }
+}
---
+++ spenn-server/src/test/kotlin/no/nav/helse/spenn/blackbox/metatest/SoapMockSimuleringTest.kt
@@ -0,0 +1,83 @@
+package no.nav.helse.spenn.blackbox.metatest
+
+import io.micrometer.prometheus.PrometheusConfig
+import io.micrometer.prometheus.PrometheusMeterRegistry
+import no.nav.helse.spenn.blackbox.soap.SoapMock
+import no.nav.helse.spenn.etEnkeltBehov
+import no.nav.helse.spenn.oppdrag.*
+import no.nav.helse.spenn.simulering.SimuleringConfig
+import no.nav.helse.spenn.simulering.SimuleringService
+import no.nav.helse.spenn.simulering.SimuleringStatus
+import org.apache.cxf.bus.extension.ExtensionManagerBus
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import java.math.BigDecimal
+import java.math.BigInteger
+import java.time.LocalDate
+import java.util.*
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class SoapMockSimuleringTest {
+
+
+    val soapMock = SoapMock(exposeTestContainerPorts = false)
+
+    private lateinit var simuleringService: SimuleringService
+
+    @BeforeAll
+    fun setup() {
+        println(soapMock.keystorePath.toAbsolutePath().toString())
+        System.setProperty("javax.net.ssl.trustStore", soapMock.keystorePath.toAbsolutePath().toString())
+        System.setProperty("javax.net.ssl.trustStorePassword", soapMock.keystorePassword)
+
+        soapMock.start()
+
+        val simuleringConfig = SimuleringConfig(
+            simuleringServiceUrl = "https://localhost:${soapMock.httpsPort}/ws/simulering",
+            stsUrl = "https://localhost:${soapMock.httpsPort}/ws/SecurityTokenService",
+            stsUsername = "hackerman",
+            stsPassword = "password123"
+        )
+
+        simuleringService = SimuleringService(
+            simuleringConfig.wrapWithSTSSimulerFpService(ExtensionManagerBus()),
+            PrometheusMeterRegistry(PrometheusConfig.DEFAULT)
+        )
+    }
+
+    @Test
+    fun testSimuleringMedSoapOgSTS() {
+        val maksDato = LocalDate.now().plusYears(1).minusDays(50)
+        val vedtakFom = LocalDate.now().minusWeeks(2)
+        val vedtakTom = LocalDate.now()
+        val enOppdragsLinje = UtbetalingsLinje(
+            id = "1234567890",
+            datoFom = vedtakFom,
+            datoTom = vedtakTom,
+            sats = BigDecimal.valueOf(1230),
+            satsTypeKode = SatsTypeKode.MÅNEDLIG,
+            utbetalesTil = "123456789",
+            grad = BigInteger.valueOf(100)
+        )
+        val utbetaling = UtbetalingsOppdrag(
+            operasjon = AksjonsKode.SIMULERING,
+            oppdragGjelder = "12121212345",
+            utbetalingsLinje = listOf(enOppdragsLinje),
+            behov = etEnkeltBehov(maksdato = maksDato)
+        )
+        val oppdragState = TransaksjonDTO(
+            id = 1L,
+            sakskompleksId = UUID.randomUUID(),
+            utbetalingsreferanse = "1001",
+            utbetalingsOppdrag = utbetaling
+        )
+
+        val nyDTO = simuleringService.runSimulering(oppdragState)
+        assertNotNull(nyDTO.simuleringResult, "simuleringresult skal ha blitt satt")
+        assertEquals(SimuleringStatus.OK, nyDTO.simuleringResult!!.status )
+
+    }
+}
