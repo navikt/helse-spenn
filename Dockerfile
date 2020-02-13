d762ee6e5661580edf005f737fcd7e617cab721f
Marthe Skogen <marthe.skogen@nav.no>
2020-02-13 15:34:24 +0100

    Legger til Jar-task i build.gradle
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM navikt/java:12
 
 COPY spenn-server/target/libs/*.jar ./
+COPY spenn-server/target/helse-spenn.jar ./app.jar
 
--- spenn-server/build.gradle.kts
+++ spenn-server/build.gradle.kts
@@ -1,3 +1,5 @@
+val mainClass = "no.nav.helse.spenn.ApplicationKt"
+
 dependencies {
     implementation(project(":spenn-core"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.61'")
@@ -5,4 +7,23 @@ dependencies {
     implementation("org.eclipse.jetty:jetty-server:9.4.19.v20190610")
     implementation("org.eclipse.jetty:jetty-webapp:9.4.19.v20190610")
     implementation("org.eclipse.jetty:jetty-servlets:9.4.19.v20190610")
+}
+
+tasks.named<Jar>("jar") {
+    baseName = "app"
+
+    manifest {
+        attributes["Main-Class"] = mainClass
+        attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+            it.name
+        }
+    }
+
+    doLast {
+        configurations.runtimeClasspath.get().forEach {
+            val file = File("$buildDir/libs/${it.name}")
+            if (!file.exists())
+                it.copyTo(file)
+        }
+    }
 }
