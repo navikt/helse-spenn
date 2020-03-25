3c3854cb7ed52c1826d150c76703cd55b7de1bd5
David Steinsland <david@steinsland.email>
2020-03-25 20:29:17 +0100

    oppdaterer rapids-biblioteket

--- spenn-server/build.gradle.kts
+++ spenn-server/build.gradle.kts
@@ -2,7 +2,7 @@ val mainClass = "no.nav.helse.spenn.ApplicationKt"
 
 dependencies {
     implementation(project(":spenn-core"))
-    implementation("com.github.navikt:rapids-and-rivers:1.1822995")
+    implementation("com.github.navikt:rapids-and-rivers:1.39bea05")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.61'")
     implementation("org.jetbrains.kotlin:kotlin-test-junit:1.3.61")
     implementation("org.eclipse.jetty:jetty-server:9.4.19.v20190610")
