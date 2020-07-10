eb4f95365c3f932b39f2da6688f568628c308354
Martin Solheim <martin.solheim@me.com>
2020-07-10 13:23:35 +0200

    Bumper dependencies og gradle versjon

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+val junitJupiterVersion = "5.6.2"
+val tjenestespesifikasjonVersion = "1.2020.06.11-19.53-1cad83414166"
+val cxfVersion = "3.3.7"
 
 plugins {
     kotlin("jvm") version "1.3.72"
@@ -10,46 +12,55 @@ buildscript {
     }
 }
 
-val junitJupiterVersion = "5.4.0"
-val tjenestespesifikasjonVersion = "1.2020.06.11-19.53-1cad83414166"
-val cxfVersion = "3.3.4"
-
 val githubUser: String by project
 val githubPassword: String by project
 
-dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:1.74ae9cb")
+repositories {
+    mavenCentral()
+    maven("https://kotlin.bintray.com/ktor")
+    maven("https://jitpack.io")
+    maven {
+        url = uri("https://maven.pkg.github.com/navikt/tjenestespesifikasjoner")
+        credentials {
+            username = githubUser
+            password = githubPassword
+        }
+    }
+}
 
-    implementation("org.flywaydb:flyway-core:6.3.1")
-    implementation("com.zaxxer:HikariCP:3.4.2")
+dependencies {
+    implementation("com.github.navikt:rapids-and-rivers:1.2954646")
+
+    implementation("org.flywaydb:flyway-core:6.5.0")
+    implementation("com.zaxxer:HikariCP:3.4.5")
     implementation("no.nav:vault-jdbc:1.3.7")
     implementation("com.github.seratch:kotliquery:1.3.1")
 
-    implementation("com.ibm.mq:com.ibm.mq.allclient:9.1.2.0")
+    implementation("com.ibm.mq:com.ibm.mq.allclient:9.1.5.0")
 
+    implementation("com.sun.xml.bind:jaxb-core:2.3.0.1")
+    implementation("com.sun.xml.bind:jaxb-impl:2.3.3")
+    implementation("com.sun.xml.messaging.saaj:saaj-impl:1.5.2")
+    implementation("javax.activation:activation:1.1.1")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("javax.jws:javax.jws-api:1.1")
     implementation("javax.validation:validation-api:2.0.1.Final")
     implementation("javax.xml.bind:jaxb-api:2.3.1")
-    implementation("com.sun.xml.bind:jaxb-core:2.3.0.1")
-    implementation("com.sun.xml.bind:jaxb-impl:2.3.2")
-    implementation("javax.annotation:javax.annotation-api:1.3.2")
-    implementation("javax.activation:activation:1.1.1")
-    implementation("org.apache.commons:commons-dbcp2:2.5.0")
     implementation("javax.xml.stream:stax-api:1.0-2")
+    implementation("javax.xml.ws:jaxws-api:2.3.1")
+    implementation("org.apache.commons:commons-dbcp2:2.7.0")
     implementation("org.apache.cxf:cxf-rt-features-logging:$cxfVersion")
     implementation("org.apache.cxf:cxf-rt-frontend-jaxws:$cxfVersion")
     implementation("org.apache.cxf:cxf-rt-transports-http:$cxfVersion")
     implementation("org.apache.cxf:cxf-rt-ws-security:$cxfVersion")
-    implementation("javax.xml.ws:jaxws-api:2.3.1")
-    implementation("javax.jws:javax.jws-api:1.1")
-    implementation("com.sun.xml.messaging.saaj:saaj-impl:1.5.1")
 
     implementation("no.nav.tjenestespesifikasjoner:avstemming-v1-tjenestespesifikasjon:$tjenestespesifikasjonVersion")
     implementation("no.nav.tjenestespesifikasjoner:nav-virksomhet-oppdragsbehandling-v1-meldingsdefinisjon:$tjenestespesifikasjonVersion")
     implementation("no.nav.tjenestespesifikasjoner:nav-system-os-simuler-fp-service-tjenestespesifikasjon:$tjenestespesifikasjonVersion")
 
     testImplementation("com.opentable.components:otj-pg-embedded:0.13.3")
-    testImplementation("io.mockk:mockk:1.9.3")
-    testImplementation("org.apache.activemq:apache-artemis:2.11.0") {
+    testImplementation("io.mockk:mockk:1.10.0")
+    testImplementation("org.apache.activemq:apache-artemis:2.13.0") {
         /* this is a shaded jar that creates conflicts on classpath, see:
             https://github.com/apache/activemq-artemis/blob/181743f3023443d9ea551164b9bbc5d366a3e38f/docs/user-manual/en/client-classpath.md
         */
@@ -69,64 +80,41 @@ configure<SourceSetContainer> {
     }
 }
 
-repositories {
-    mavenCentral()
-    maven("https://kotlin.bintray.com/ktor")
-    maven {
-        url = uri("https://maven.pkg.github.com/navikt/rapids-and-rivers")
-        credentials {
-            username = githubUser
-            password = githubPassword
-        }
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "12"
     }
-    maven {
-        url = uri("https://maven.pkg.github.com/navikt/tjenestespesifikasjoner")
-        credentials {
-            username = githubUser
-            password = githubPassword
-        }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "12"
     }
-}
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_12
-    targetCompatibility = JavaVersion.VERSION_12
-}
+    named<Jar>("jar") {
+        archiveFileName.set("app.jar")
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "12"
-}
+        manifest {
+            attributes["Main-Class"] = "no.nav.helse.spenn.ApplicationKt"
+            attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
+                it.name
+            }
+        }
 
-tasks.named<KotlinCompile>("compileTestKotlin") {
-    kotlinOptions.jvmTarget = "12"
-}
-
-tasks.named<Jar>("jar") {
-    archiveFileName.set("app.jar")
-    manifest {
-        attributes["Main-Class"] = "no.nav.helse.spenn.ApplicationKt"
-        attributes["Class-Path"] = configurations.runtimeClasspath.get().joinToString(separator = " ") {
-            it.name
+        doLast {
+            configurations.runtimeClasspath.get().forEach {
+                val file = File("$buildDir/libs/${it.name}")
+                if (!file.exists())
+                    it.copyTo(file)
+            }
         }
     }
 
-    doLast {
-        configurations.runtimeClasspath.get().forEach {
-            val file = File("$buildDir/libs/${it.name}")
-            if (!file.exists())
-                it.copyTo(file)
+    withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            events("passed", "skipped", "failed")
         }
     }
-}
 
-
-tasks.withType<Test> {
-    useJUnitPlatform()
-    testLogging {
-        events("passed", "skipped", "failed")
+    withType<Wrapper> {
+        gradleVersion = "6.5.1"
     }
-}
-
-tasks.withType<Wrapper> {
-    gradleVersion = "5.6.4"
 }
