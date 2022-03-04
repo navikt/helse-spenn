864d1809a4f08c06ae3cd279a6d1769d25dcdcc1
Erik M.Forsman <erik.maximilian.forsman@nav.no>
2022-03-04 14:07:20 +0100

    Legger til mappingtest fra xml-response
    * Også test for desimalverdi på totalBeløp

--- src/test/kotlin/no/nav/helse/spenn/simulering/SimuleringServiceTest.kt
+++ src/test/kotlin/no/nav/helse/spenn/simulering/SimuleringServiceTest.kt
@@ -11,12 +11,12 @@ import no.nav.system.os.entiteter.beregningskjema.BeregningsPeriode
 import no.nav.system.os.tjenester.simulerfpservice.feil.FeilUnderBehandling
 import no.nav.system.os.tjenester.simulerfpservice.simulerfpservicegrensesnitt.SimulerBeregningRequest
 import no.nav.system.os.tjenester.simulerfpservice.simulerfpserviceservicetypes.SimulerBeregningResponse
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import java.math.BigDecimal
 import java.math.BigInteger
+import java.time.LocalDate
 
 internal class SimuleringServiceTest {
     private val simulerFpService = mockk<SimulerFpService>()
@@ -33,11 +33,60 @@ internal class SimuleringServiceTest {
 
     @Test
     fun `simulering med feil under behandling`() {
-        every {
-            simulerFpService.simulerBeregning(any())
-        } throws SimulerBeregningFeilUnderBehandling("Helt feil", FeilUnderBehandling())
+        every { simulerFpService.simulerBeregning(any()) } throws SimulerBeregningFeilUnderBehandling(
+            "Helt feil", FeilUnderBehandling()
+        )
         val result = simuleringService.simulerOppdrag(SimulerBeregningRequest())
         assertEquals(SimuleringStatus.FUNKSJONELL_FEIL, result.status)
+    }
+
+    @Test
+    fun `mapping av simuleringsresponse`() {
+        val forventet = SimuleringResult(
+            status = SimuleringStatus.OK,
+            feilmelding = null,
+            simulering = Simulering(
+                gjelderId = "gjelderId",
+                gjelderNavn = "gjelderNavn",
+                datoBeregnet = LocalDate.parse("2020-01-01"),
+                totalBelop = 4500,
+                periodeList = listOf(
+                    SimulertPeriode(
+                        fom = LocalDate.parse("2020-01-02"),
+                        tom = LocalDate.parse("2020-03-02"),
+                        utbetaling = listOf(
+                            Utbetaling(
+                                fagSystemId = "fagSystemId",
+                                utbetalesTilId = "utbetalesTilId",
+                                utbetalesTilNavn = "utbetalesTilNavn",
+                                forfall = LocalDate.parse("2022-01-01"),
+                                feilkonto = true,
+                                detaljer = listOf(
+                                    Detaljer(
+                                        faktiskFom = LocalDate.parse("2020-01-03"),
+                                        faktiskTom = LocalDate.parse("2020-03-03"),
+                                        konto = "kontoStreng",
+                                        belop = 3200,
+                                        tilbakeforing = true,
+                                        sats = 1,
+                                        typeSats = "typeSats",
+                                        antallSats = 2,
+                                        uforegrad = 400,
+                                        klassekode = "klassekode",
+                                        klassekodeBeskrivelse = "klasseKodeBeskrivelse",
+                                        utbetalingsType = "typeKlasse",
+                                        refunderesOrgNr = "refunderesOrgNr"
+                                    )
+                                )
+                            )
+                        )
+                    )
+                )
+            )
+        )
+        every { simulerFpService.simulerBeregning(any()) } returns lagSimulerBeregningResponse()
+        val faktisk = simuleringService.simulerOppdrag(SimulerBeregningRequest())
+        assertEquals(forventet, faktisk)
     }
 
     @Test
@@ -67,6 +116,14 @@ internal class SimuleringServiceTest {
     }
 
     @Test
+    fun `simuleringsrespons med desimal totalbeløp`() {
+        every { simulerFpService.simulerBeregning(any()) } returns lagSimulerBeregningResponse(
+            totalBeløp = BigDecimal.valueOf(500.5)
+        )
+        assertThrows<ArithmeticException> { simuleringService.simulerOppdrag(SimulerBeregningRequest()) }
+    }
+
+    @Test
     fun `simuleringsrespons med desimal antallSats`() {
         every { simulerFpService.simulerBeregning(any()) } returns lagSimulerBeregningResponse(
             antallSats = BigDecimal.valueOf(1.5)
@@ -75,9 +132,10 @@ internal class SimuleringServiceTest {
     }
 
     private fun lagSimulerBeregningResponse(
-        beløp: BigDecimal = BigDecimal.valueOf(1),
+        totalBeløp: BigDecimal = BigDecimal.valueOf(4500),
+        beløp: BigDecimal = BigDecimal.valueOf(3200),
         sats: BigDecimal = BigDecimal.valueOf(1),
-        antallSats: BigDecimal = BigDecimal.valueOf(1)
+        antallSats: BigDecimal = BigDecimal.valueOf(2)
     ) = no.nav.system.os.tjenester.simulerfpservice.simulerfpservicegrensesnitt.SimulerBeregningResponse().apply {
         response = SimulerBeregningResponse().apply {
             simulering = Beregning().apply {
@@ -85,38 +143,38 @@ internal class SimuleringServiceTest {
                 gjelderNavn = "gjelderNavn"
                 datoBeregnet = "2020-01-01"
                 kodeFaggruppe = "kodeFaggruppe"
-                belop = BigDecimal(4500)
+                belop = totalBeløp
                 beregningsPeriode.add(BeregningsPeriode().apply {
-                    periodeFom = "2020-01-01"
-                    periodeTom = "2020-03-01"
+                    periodeFom = "2020-01-02"
+                    periodeTom = "2020-03-02"
                     beregningStoppnivaa.add(BeregningStoppnivaa().apply {
                         kodeFagomraade = "kodeFagomraade"
-                        stoppNivaaId = BigInteger.valueOf(500)
+                        stoppNivaaId = BigInteger.valueOf(100)
                         behandlendeEnhet = "behandlendeEnhet"
-                        oppdragsId = 1337
-                        fagsystemId = "fagsystemId"
+                        oppdragsId = 200
+                        fagsystemId = "fagSystemId"
                         kid = "kid"
                         utbetalesTilId = "utbetalesTilId"
                         utbetalesTilNavn = "utbetalesTilNavn"
                         bilagsType = "bilagsType"
                         forfall = "2022-01-01"
-                        isFeilkonto = false
+                        isFeilkonto = true
                         beregningStoppnivaaDetaljer.add(BeregningStoppnivaaDetaljer().apply {
-                            faktiskFom = "2020-01-01"
-                            faktiskTom = "2020-03-01"
+                            faktiskFom = "2020-01-03"
+                            faktiskTom = "2020-03-03"
                             kontoStreng = "kontoStreng"
                             behandlingskode = "behandlingskode"
                             belop = beløp
                             trekkVedtakId = 1
                             stonadId = "stonadId"
                             korrigering = "korrigering"
-                            isTilbakeforing = false
-                            linjeId = BigInteger.valueOf(1)
+                            isTilbakeforing = true
+                            linjeId = BigInteger.valueOf(300)
                             this.sats = sats
                             typeSats = "typeSats"
                             this.antallSats = antallSats
                             saksbehId = "saksbehId"
-                            uforeGrad = BigInteger.valueOf(1)
+                            uforeGrad = BigInteger.valueOf(400)
                             kravhaverId = "kravhaverId"
                             delytelseId = "delytelseId"
                             bostedsenhet = "bostedsenhet"
