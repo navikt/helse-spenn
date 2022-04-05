5132a27abc0a12903e5b00887043b8a05520e3ae
David Steinsland <david@steinsland.email>
2022-04-05 09:52:12 +0200

    bumper rapids-and-rivers

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:2022.04.05-08.01.237f724ae572")
+    implementation("com.github.navikt:rapids-and-rivers:2022.04.05-09.40.11a466d7ac70")
     implementation("org.jetbrains.kotlin:kotlin-reflect:1.5.31")
 
     implementation("org.flywaydb:flyway-core:8.0.2")
