9f8b87ee1bcef18e46c75134df548e0213115fb0
David Steinsland <david@steinsland.email>
2022-10-31 22:26:34 +0100

    fikser miljøvariabler for gcp

--- spenn-opprydding-dev/src/main/kotlin/no.nav.helse.opprydding/DataSourceBuilder.kt
+++ spenn-opprydding-dev/src/main/kotlin/no.nav.helse.opprydding/DataSourceBuilder.kt
@@ -8,7 +8,7 @@ import javax.sql.DataSource
 
 // Understands how to create a data source from environment variables
 internal class DataSourceBuilder(env: Map<String, String>) {
-    private val databaseName = env["DATABASE_NAME"] ?: env["DATABASE_SPENN_OPPRYDDING_DEV_DATABASE"]
+    private val databaseName = env["DATABASE_NAME"] ?: env["DATABASE_SPENN_OPPRYDDING_DATABASE"]
 
     private val vaultMountPath = env["VAULT_MOUNTPATH"]
     private val shouldGetCredentialsFromVault = vaultMountPath != null
@@ -18,13 +18,13 @@ internal class DataSourceBuilder(env: Map<String, String>) {
     private val hikariConfig = HikariConfig().apply {
         jdbcUrl = env["DATABASE_JDBC_URL"] ?: String.format(
             "jdbc:postgresql://%s:%s/%s%s",
-            requireNotNull(env["DATABASE_HOST"] ?: env["DATABASE_SPENN_OPPRYDDING_DEV_HOST"]) { "database host must be set if jdbc url is not provided" },
-            requireNotNull(env["DATABASE_PORT"] ?: env["DATABASE_SPENN_OPPRYDDING_DEV_PORT"]) { "database port must be set if jdbc url is not provided" },
+            requireNotNull(env["DATABASE_HOST"] ?: env["DATABASE_SPENN_OPPRYDDING_HOST"]) { "database host must be set if jdbc url is not provided" },
+            requireNotNull(env["DATABASE_PORT"] ?: env["DATABASE_SPENN_OPPRYDDING_PORT"]) { "database port must be set if jdbc url is not provided" },
             requireNotNull(databaseName) { "database name must be set if jdbc url is not provided" },
-            (env["DATABASE_USERNAME"] ?: env["DATABASE_SPENN_OPPRYDDING_DEV_USERNAME"])?.let { "?user=$it" } ?: "")
+            (env["DATABASE_USERNAME"] ?: env["DATABASE_SPENN_OPPRYDDING_USERNAME"])?.let { "?user=$it" } ?: "")
 
-        (env["DATABASE_USERNAME"] ?: env["DATABASE_SPENN_OPPRYDDING_DEV_USERNAME"])?.let { this.username = it }
-        (env["DATABASE_PASSWORD"] ?: env["DATABASE_SPENN_OPPRYDDING_DEV_PASSWORD"])?.let { this.password = it }
+        (env["DATABASE_USERNAME"] ?: env["DATABASE_SPENN_OPPRYDDING_USERNAME"])?.let { this.username = it }
+        (env["DATABASE_PASSWORD"] ?: env["DATABASE_SPENN_OPPRYDDING_PASSWORD"])?.let { this.password = it }
 
         maximumPoolSize = 3
         connectionTimeout = Duration.ofSeconds(30).toMillis()
@@ -35,8 +35,8 @@ internal class DataSourceBuilder(env: Map<String, String>) {
     init {
         if (!shouldGetCredentialsFromVault) {
             if (!env.containsKey("DATABASE_JDBC_URL")) {
-                checkNotNull(env["DATABASE_USERNAME"] ?: env["DATABASE_SPENN_OPPRYDDING_DEV_USERNAME"]) { "username must be set when vault is disabled" }
-                checkNotNull(env["DATABASE_PASSWORD"] ?: env["DATABASE_SPENN_OPPRYDDING_DEV_PASSWORD"]) { "password must be set when vault is disabled" }
+                checkNotNull(env["DATABASE_USERNAME"] ?: env["DATABASE_SPENN_OPPRYDDING_USERNAME"]) { "username must be set when vault is disabled" }
+                checkNotNull(env["DATABASE_PASSWORD"] ?: env["DATABASE_SPENN_OPPRYDDING_PASSWORD"]) { "password must be set when vault is disabled" }
             }
         } else {
             check(null == env["DATABASE_USERNAME"]) { "username must not be set when vault is enabled" }
