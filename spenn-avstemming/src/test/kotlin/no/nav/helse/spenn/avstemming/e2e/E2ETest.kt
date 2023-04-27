1423eadde908b7a2d1856582c4eb830bd9920822
David Steinsland <david@steinsland.email>
2023-04-27 13:44:31 +0200

    utvider test med flere meldinger

--- spenn-avstemming/src/test/kotlin/no/nav/helse/spenn/avstemming/e2e/E2ETest.kt
+++ spenn-avstemming/src/test/kotlin/no/nav/helse/spenn/avstemming/e2e/E2ETest.kt
@@ -1,20 +1,25 @@
 package no.nav.helse.spenn.avstemming.e2e
 
+import com.fasterxml.jackson.databind.node.ObjectNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNull
 import kotlinx.coroutines.runBlocking
 import kotliquery.queryOf
 import kotliquery.sessionOf
 import kotliquery.using
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import no.nav.helse.spenn.avstemming.Database
+import no.nav.helse.spenn.avstemming.Oppdragstatus
 import no.nav.helse.spenn.avstemming.UtKø
 import no.nav.helse.spenn.avstemming.rapidApp
 import org.flywaydb.core.Flyway
 import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
 import org.testcontainers.containers.PostgreSQLContainer
 import java.time.LocalDateTime
 import java.util.*
@@ -24,6 +29,7 @@ class E2ETest {
 
     private val testRapid = TestRapid()
     private val database = TestDatabase()
+    private val mapper = jacksonObjectMapper()
     private val utkø = object : UtKø {
         override fun send(messageString: String) {}
     }
@@ -49,8 +55,18 @@ class E2ETest {
         val totalbeløp = 5000
         val opprettet = LocalDateTime.now()
 
-        testRapid.sendTestMessage(oppdragutbetaling(avstemmingsnøkkel, utbetalingId, fagsystemId, fagområde, fødselsnummer, mottaker, totalbeløp, opprettet))
+        val oppdragutbetaling = oppdragutbetaling(avstemmingsnøkkel, utbetalingId, fagsystemId, fagområde, fødselsnummer, mottaker, totalbeløp, opprettet)
+        testRapid.sendTestMessage(oppdragutbetaling)
         assertEquals(1, database.antallOppdrag())
+        assertNull(database.status(avstemmingsnøkkel))
+
+        testRapid.sendTestMessage(oppdragutbetaling.medKvittering("OVERFØRT"))
+        assertEquals(1, database.antallOppdrag())
+        assertEquals(Oppdragstatus.MANGELFULL, database.status(avstemmingsnøkkel))
+
+        testRapid.sendTestMessage(transaksjonStatus(avstemmingsnøkkel, fødselsnummer, fagsystemId, "00", "AKSEPTERT", null, null, "xml"))
+        assertEquals(1, database.antallOppdrag())
+        assertEquals(Oppdragstatus.AKSEPTERT, database.status(avstemmingsnøkkel))
     }
 
     @Language("JSON")
@@ -73,24 +89,36 @@ class E2ETest {
 
 
     @Language("JSON")
-    private fun oppdragutbetalingMedKvittering(avstemmingsnøkkel: Long, utbetalingId: UUID, fagsystemId: String, fagområde: String, fødselsnummer: String, mottaker: String, totalbeløp: Int, opprettet: LocalDateTime) = """
-    {
-      "@event_name": "oppdrag_utbetaling",
-      "@id": "${UUID.randomUUID()}",
-      "@opprettet": "${LocalDateTime.now()}",
-      "fødselsnummer": "$fødselsnummer",
-      "aktørId": "aktør",
-      "utbetalingId": "$utbetalingId",
-      "fagsystemId": "$fagsystemId",
-      "fagområde": "$fagområde",
-      "mottaker": "$mottaker",
-      "opprettet": "$opprettet",
-      "avstemmingsnøkkel": $avstemmingsnøkkel,
-      "totalbeløp": $totalbeløp,
-      "kvittering": {
-        "status": "OVERFØRT"
-      }
-    }
+    private fun String.medKvittering(status: String) = (mapper.readTree(this) as ObjectNode).apply {
+        putObject("kvittering").apply {
+            put("status", status)
+        }
+    }.toString()
+
+    @Language("JSON")
+    private fun transaksjonStatus(
+        avstemmingsnøkkel: Long,
+        fødselsnummer: String,
+        fagsystemId: String,
+        alvorlighetsgrad: String,
+        status: String,
+        kodemelding: String?,
+        beskrivendemelding: String?,
+        oppdragkvittering: String
+    ) = """
+        {
+          "@event_name": "transaksjon_status",
+          "@id": "${UUID.randomUUID()}",
+          "@opprettet": "${LocalDateTime.now()}",
+          "fødselsnummer": "$fødselsnummer",
+          "avstemmingsnøkkel": $avstemmingsnøkkel,
+          "fagsystemId": "$fagsystemId",
+          "feilkode_oppdrag": "$alvorlighetsgrad",
+          "status": "$status",
+          "kodemelding": ${kodemelding?.let { "\"$it\"" } ?: "null"},
+          "beskrivendemelding": ${beskrivendemelding?.let { "\"$it\"" } ?: "null"},
+          "originalXml": "$oppdragkvittering"
+        }
     """
 
     private class TestDatabase : Database {
@@ -127,5 +155,9 @@ class E2ETest {
         fun antallOppdrag() = sessionOf(getDataSource()).use {
             it.run(queryOf("SELECT COUNT(1) FROM oppdrag").map { it.int(1) }.asSingle)
         } ?: 0
+
+        fun status(avstemmingsnøkkel: Long) = sessionOf(getDataSource()).use {
+            it.run(queryOf("SELECT status FROM oppdrag WHERE avstemmingsnokkel=?", avstemmingsnøkkel).map { it.stringOrNull("status")?.let { Oppdragstatus.valueOf(it) } }.asSingle)
+        }
     }
 }
