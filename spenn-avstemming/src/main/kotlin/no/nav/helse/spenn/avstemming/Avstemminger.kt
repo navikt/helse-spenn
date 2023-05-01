f700fd6029a291606f0655352f05e4dd804f10ff
David Steinsland <david@steinsland.email>
2023-05-01 16:00:11 +0200

    avtemmer vha. 'hel time'-eventet
    frikobler oss fra å ha en cronjob som må kjøre kl 9 for å starte avstemming
    
    beholder river for spisset 'utfør_avstemming'-event likevel, ettersom det kan være nyttig om vi skal trigge avstemming manuelt

--- spenn-avstemming/src/main/kotlin/no/nav/helse/spenn/avstemming/Avstemminger.kt
+++ spenn-avstemming/src/main/kotlin/no/nav/helse/spenn/avstemming/Avstemminger.kt
@@ -3,17 +3,15 @@ package no.nav.helse.spenn.avstemming
 import com.fasterxml.jackson.databind.JsonNode
 import no.nav.helse.rapids_rivers.*
 import no.nav.virksomhet.tjenester.avstemming.meldinger.v1.Avstemmingsdata
-import org.apache.kafka.clients.producer.ProducerRecord
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.util.*
-import kotlin.math.log
 
 internal class Avstemminger(
     rapidsConnection: RapidsConnection,
-    oppdragDao: OppdragDao,
-    avstemmingDao: AvstemmingDao,
-    tilAvstemming: UtKø,
+    private val oppdragDao: OppdragDao,
+    private val avstemmingDao: AvstemmingDao,
+    private val utkø: UtKø,
 ) {
     private companion object {
         private val logger = Logg.ny(Avstemminger::class)
@@ -25,14 +23,18 @@ internal class Avstemminger(
                 it.demandValue("@event_name", "utfør_avstemming")
                 it.require("dagen", JsonNode::asLocalDate)
             }
-        }.register(UtførAvstemming(oppdragDao, avstemmingDao, tilAvstemming))
+        }.register(UtførAvstemming())
+        River(rapidsConnection).apply {
+            validate {
+                it.demandValue("@event_name", "hel_time")
+                it.requireValue("time", "9")
+                it.require("dagen", JsonNode::asLocalDate)
+            }
+        }.register(AvstemmingBegivenhet())
     }
 
-    private class UtførAvstemming(
-        private val oppdragDao: OppdragDao,
-        private val avstemmingDao: AvstemmingDao,
-        private val utkø: UtKø
-    ) : River.PacketListener {
+    // utfører avstemming fra spisset signal, f.eks. fra spout
+    private inner class UtførAvstemming : River.PacketListener {
         override fun onError(problems: MessageProblems, context: MessageContext) {
             logger
                 .offentligError("Forstod ikke utfør_avstemming (se sikkerlogg for detaljer)")
@@ -41,73 +43,91 @@ internal class Avstemminger(
 
         override fun onPacket(packet: JsonMessage, context: MessageContext) {
             val dagen = packet["dagen"].asLocalDate()
-            val avstemmingsnøkkelTom = Avstemmingsnøkkel.tilOgMed(dagen)
-            logger.info("forbereder avstemming av $dagen frem til og med avstemmingsnøkkel=$avstemmingsnøkkelTom")
+            avstem(dagen, context)
+        }
+    }
 
-            val fagområder = oppdragDao.hentOppdragForAvstemming(avstemmingsnøkkelTom)
-            if (fagområder.isEmpty()) {
-                logger.info("det var ingenting å avstemme")
-                return
-            }
-            fagområder.forEach { (fagområde, oppdrag) ->
-                val id = UUID.randomUUID()
-                logger.info("Starter avstemming id=$id fagområde=$fagområde frem til og med $dagen")
-                avstemOppdrag(context, id, dagen, fagområde, oppdrag)
-                logger.info("Avstemming id=$id fagområde=$fagområde frem til og med $dagen er ferdig")
-            }
+    private inner class AvstemmingBegivenhet : River.PacketListener {
+        override fun onError(problems: MessageProblems, context: MessageContext) {
+            logger
+                .offentligError("Forstod ikke hel_time (se sikkerlogg for detaljer)")
+                .privatError("Forstod ikke hel_time:\n${problems.toExtendedReport()}")
         }
 
-        private fun avstemOppdrag(
-            context: MessageContext,
-            id: UUID,
-            dagen: LocalDate,
-            fagområde: String,
-            oppdrag: List<OppdragDto>
-        ) {
-            if (oppdrag.isEmpty()) {
-                logger.info("ingenting å avstemme for fagområde $fagområde")
-                return
-            }
-            val avstemmingsperiode = OppdragDto.avstemmingsperiode(oppdrag)
-            val meldinger = AvstemmingBuilder(id, fagområde, oppdrag).build()
-            avstemmingDao.nyAvstemming(id, fagområde, avstemmingsperiode.endInclusive, oppdrag.size)
-            logger.info("avstemmer nøkkelFom=${avstemmingsperiode.start} nøkkelTom=${avstemmingsperiode.endInclusive}: sender ${meldinger.size} meldinger")
-            meldinger.forEach { sendAvstemmingsmelding(it) }
-            oppdragDao.oppdaterAvstemteOppdrag(fagområde, avstemmingsperiode.endInclusive)
-            val avstemminginfo =
-                mapOf(
-                    "nøkkel_fom" to avstemmingsperiode.start,
-                    "nøkkel_tom" to avstemmingsperiode.endInclusive,
-                    "antall_oppdrag" to oppdrag.size,
-                    "antall_avstemmingsmeldinger" to meldinger.size
-                )
-
-            context.publish(avstemmingevent(id, dagen, fagområde, oppdrag.size, avstemminginfo).toJson().also {
-                logger.info("sender $it")
-            })
+        override fun onPacket(packet: JsonMessage, context: MessageContext) {
+            // avstemmer gårsdagen
+            val dagen = packet["dagen"].asLocalDate().minusDays(1)
+            avstem(dagen, context)
         }
+    }
 
-        private fun avstemmingevent(
-            id: UUID,
-            dagen: LocalDate,
-            fagområde: String,
-            antallOppdrag: Int,
-            avstemminginfo: Map<String, Any>
-        ) = JsonMessage.newMessage(
+    private fun avstem(dagen: LocalDate, context: MessageContext) {
+        val avstemmingsnøkkelTom = Avstemmingsnøkkel.tilOgMed(dagen)
+        logger.info("forbereder avstemming av $dagen frem til og med avstemmingsnøkkel=$avstemmingsnøkkelTom")
+
+        val fagområder = oppdragDao.hentOppdragForAvstemming(avstemmingsnøkkelTom)
+        if (fagområder.isEmpty()) {
+            logger.info("det var ingenting å avstemme")
+            return
+        }
+        fagområder.forEach { (fagområde, oppdrag) ->
+            val id = UUID.randomUUID()
+            logger.info("Starter avstemming id=$id fagområde=$fagområde frem til og med $dagen")
+            avstemOppdrag(context, id, dagen, fagområde, oppdrag)
+            logger.info("Avstemming id=$id fagområde=$fagområde frem til og med $dagen er ferdig")
+        }
+    }
+
+    private fun avstemOppdrag(
+        context: MessageContext,
+        id: UUID,
+        dagen: LocalDate,
+        fagområde: String,
+        oppdrag: List<OppdragDto>
+    ) {
+        if (oppdrag.isEmpty()) {
+            logger.info("ingenting å avstemme for fagområde $fagområde")
+            return
+        }
+        val avstemmingsperiode = OppdragDto.avstemmingsperiode(oppdrag)
+        val meldinger = AvstemmingBuilder(id, fagområde, oppdrag).build()
+        avstemmingDao.nyAvstemming(id, fagområde, avstemmingsperiode.endInclusive, oppdrag.size)
+        logger.info("avstemmer nøkkelFom=${avstemmingsperiode.start} nøkkelTom=${avstemmingsperiode.endInclusive}: sender ${meldinger.size} meldinger")
+        meldinger.forEach { sendAvstemmingsmelding(it) }
+        oppdragDao.oppdaterAvstemteOppdrag(fagområde, avstemmingsperiode.endInclusive)
+        val avstemminginfo =
             mapOf(
-                "@event_name" to "avstemming",
-                "@id" to id,
-                "@opprettet" to LocalDateTime.now(),
-                "dagen" to dagen,
-                "fagområde" to fagområde,
-                "antall_oppdrag" to antallOppdrag,
-                "detaljer" to avstemminginfo
+                "nøkkel_fom" to avstemmingsperiode.start,
+                "nøkkel_tom" to avstemmingsperiode.endInclusive,
+                "antall_oppdrag" to oppdrag.size,
+                "antall_avstemmingsmeldinger" to meldinger.size
             )
-        )
 
-        private fun sendAvstemmingsmelding(melding: Avstemmingsdata) {
-            val xmlMelding = AvstemmingdataXml.marshal(melding)
-            utkø.send(xmlMelding)
-        }
+        context.publish(avstemmingevent(id, dagen, fagområde, oppdrag.size, avstemminginfo).toJson().also {
+            logger.info("sender $it")
+        })
+    }
+
+    private fun avstemmingevent(
+        id: UUID,
+        dagen: LocalDate,
+        fagområde: String,
+        antallOppdrag: Int,
+        avstemminginfo: Map<String, Any>
+    ) = JsonMessage.newMessage(
+        mapOf(
+            "@event_name" to "avstemming",
+            "@id" to id,
+            "@opprettet" to LocalDateTime.now(),
+            "dagen" to dagen,
+            "fagområde" to fagområde,
+            "antall_oppdrag" to antallOppdrag,
+            "detaljer" to avstemminginfo
+        )
+    )
+
+    private fun sendAvstemmingsmelding(melding: Avstemmingsdata) {
+        val xmlMelding = AvstemmingdataXml.marshal(melding)
+        utkø.send(xmlMelding)
     }
 }
